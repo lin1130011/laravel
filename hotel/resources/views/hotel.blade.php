8cf9a5737a076e5aae31371f0a9e513c3184f02a
lin1130011 <lin1130011@gmail.com>
2024-08-20 16:33:28 +0800

    專案練習

---
+++ hotel/resources/views/hotel.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <a href="{{ route('f1name') }}">F1</a><br>
+    <a href="{{ route('f2name') }}">F2</a><br>
+    <a href="{{ route('f3name') }}">F3</a><br>
+    <a href="{{ route('cars') }}">cars</a><br>
+    <a href="{{ route('students') }}">學生</a><br>
+
+
+</body>
+
+</html>
---
+++ hotel/resources/views/test.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <p>num1 = {{ $watch['num1'] }}</p>
+    <p>num2 = {{ $watch['num2'] }}</p>
+    <p>sum = {{ $watch['sum'] }}</p>
+</body>
+
+</html>
